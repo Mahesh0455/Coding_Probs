72adf83d1247689ad4c558bcd251072513364f75
Mahesh Pawar <mahesh.p.pawar045@gmail.com>
2022-10-10 21:18:48 +0530

    basic_codes

---
+++ src/basic/P1.java
@@ -0,0 +1,29 @@
+package basic;
+
+import java.util.Scanner;
+
+/*Read and Print an Integer value in Java
+*/
+public class P1 {
+	
+	
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("ENter a number");
+		
+		int a=sc.nextInt();
+		
+		System.out.println("The NUmber entered is: "+a);
+		System.out.println("ENter a String");
+		String s=sc.nextLine();
+		System.out.println("Entered String is: "+s);
+		
+		
+		
+		
+	}
+	
+	
+
+}
